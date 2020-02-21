7c840e778294a4b1ee9d4d3e836b358da0426d63
thanhtan28011998 <thanhtan28011998@gmail.com>
2020-02-21 10:52:24 +0700

    Updates

---
+++ precache-manifest.eaa240284abad0caf2b96925f114777b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c08d7a17753e3bedc208f4c95890048",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "72a1081ea2d0bc3c8056",
+    "url": "/calculator-react/static/css/main.8ecaf228.chunk.css"
+  },
+  {
+    "revision": "f056c870b2f497933cbc",
+    "url": "/calculator-react/static/js/2.4a59d197.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator-react/static/js/2.4a59d197.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72a1081ea2d0bc3c8056",
+    "url": "/calculator-react/static/js/main.a2bd2b11.chunk.js"
+  },
+  {
+    "revision": "93a15371a85e7ff70d19",
+    "url": "/calculator-react/static/js/runtime-main.217d35fc.js"
+  }
+]);
